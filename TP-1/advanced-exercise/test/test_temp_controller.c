14f06fbd7b72d06fbb6e7288bfbba9dbb83d3c84
Onsarouri <ons.arourii@gmail.com>
2024-12-26 19:41:40 +0100

    commit

--- TP-1/advanced-exercise/test/test_temp_controller.c
+++ TP-1/advanced-exercise/test/test_temp_controller.c
@@ -4,37 +4,39 @@
 
 void setUp(void) {
     // Initialisez les mocks "Bonne pratique"
-    // ??
+    mock_temp_sensor_Init();
 }
 
 void tearDown(void) {
     // Nettoiez les mocks "Bonne pratique"
     // ??
+    mock_temp_sensor_Verify();
+    mock_temp_sensor_Destroy();
 }
 
 void test_check_temperature_should_return_1_when_too_hot(void) {
-    read_temperature_sensor_ExpectAndReturn(/* Température élevée */);
-    TEST_ASSERT_EQUAL(/* Résultat attendu */, check_temperature());
+    read_temperature_sensor_ExpectAndReturn(31);
+    TEST_ASSERT_EQUAL(1, check_temperature());
 }
 
 void test_check_temperature_should_return_minus_1_when_too_cold(void) {
-    read_temperature_sensor_ExpectAndReturn(/* Température basse */);
-    TEST_ASSERT_EQUAL(/* Résultat attendu */, check_temperature());
+    read_temperature_sensor_ExpectAndReturn(10);
+    TEST_ASSERT_EQUAL(-1, check_temperature());
 }
 
 void test_check_temperature_should_return_0_when_normal(void) {
-    read_temperature_sensor_ExpectAndReturn(/* Température normale */);
-    TEST_ASSERT_EQUAL(/* Résultat attendu */, check_temperature());
+    read_temperature_sensor_ExpectAndReturn(22);
+    TEST_ASSERT_EQUAL(0, check_temperature());
 }
 
 
 // Testes supplémentaires : Cas limites exacts
 void test_check_temperature_should_handle_exact_limits(void) {
-    read_temperature_sensor_ExpectAndReturn(/* ?? */); // Limite basse
-    TEST_ASSERT_EQUAL(/* ?? */, check_temperature());
+    read_temperature_sensor_ExpectAndReturn(15); // Limite basse
+    TEST_ASSERT_EQUAL(0, check_temperature());
 
-    read_temperature_sensor_ExpectAndReturn(/* ?? */); // Limite haute
-    TEST_ASSERT_EQUAL(/* ?? */, check_temperature());
+    read_temperature_sensor_ExpectAndReturn(30); // Limite haute
+    TEST_ASSERT_EQUAL(0, check_temperature());
 
     /* ++ Vous pouvez ajouter d'autres tests pour explorer les limites ++ */
 }
@@ -47,3 +49,12 @@ void test_check_temperature_should_handle_exact_limits(void) {
     - Étape 2 : Créez un test unitaire pour valider ce comportement.
 */
 
+void test_check_temperature_should_handle_extreme_values(void) {
+    
+    read_temperature_sensor_ExpectAndReturn(-50); // Hors gamme
+    TEST_ASSERT_EQUAL(-2, check_temperature()); // Retour attendu pour une valeur hors gamme
+
+    
+    read_temperature_sensor_ExpectAndReturn(220); // Hors gamme
+    TEST_ASSERT_EQUAL(-2, check_temperature()); 
+}
--- TP-1/test/test_calcul.c
+++ TP-1/test/test_calcul.c
@@ -2,11 +2,10 @@
 #include "calcul.h"
 
 void test_addition(void) {
-    TEST_ASSERT_EQUAL_INT(5, addition(2, 3));
-    TEST_ASSERT_EQUAL_INT(17, addition(10, -7));
+    TEST_ASSERT_EQUAL_INT(5, addition(1, 3));
+    TEST_ASSERT_EQUAL_INT(3, addition(10, -7));
     // Complétez ici avec des cas spécifiques
-    //TEST_ASSERT_EQUAL_INT(X, addition("""Completez ici"""));
-    //TEST_ASSERT_EQUAL_INT(X, addition("""Completez ici"""));
+    TEST_ASSERT_EQUAL_INT(-14, addition(-7, -7));
+    TEST_ASSERT_EQUAL_INT(0, addition(0, -1));
     //TEST_ASSERT_EQUAL_INT(X, addition("""Completez ici"""));
 }
-
